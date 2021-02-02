68ec9f666476a75f3aeafdaac1c756a743dca187
champoyradoo <champoyradoo@gmail.com>
2021-02-02 23:21:20 +0800

    Static photo show page

--- src/pages/Photos/PhotoShow.vue
+++ src/pages/Photos/PhotoShow.vue
@@ -1,10 +1,21 @@
 <template>
-    <div></div>
+    <q-page class="text-center">
+        <h1 class="q-mb-sm">{{ photo.title }}</h1>
+
+        <q-img :src="photo.url" style="max-width: 600px;" />
+    </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PhotoShow.vue'
+  name: 'PhotoShow.vue',
+
+  data: () => ({
+    photo: {
+      title: 'Some Title',
+      url: 'https://via.placeholder.com/600/771796'
+    }
+  })
 }
 </script>
 
